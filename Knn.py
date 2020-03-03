f3e34046b8652c9aef1b4c3f048d534db70ace65
CuiZhenKai <626512587@qq.com>
2020-03-03 14:24:28 +0800

    KNN实现

---
+++ Knn.py
@@ -0,0 +1,53 @@
+import numpy as np
+from collections import Counter
+from math import sqrt
+
+class KNeighborsClassifier:
+
+    def __init__(self,k):
+        '''check'''
+        assert k>=1,"K must be valid"
+        self.k = k
+
+        '''private'''
+        self._X_train = None
+        self._y_train = None
+
+    def fit(self,X,y):
+        '''check'''
+        assert X.shape[0] == y.shape[0],\
+                "The data size must be valid"
+        assert self.k <= y.shape[0],\
+                "K must be valid"
+
+        self._X_train = X
+        self._y_train = y
+
+        '''according to the scikit-learn,we need to return self'''
+        return self
+
+    def predict(self,x_predict):
+        '''check'''
+        assert self._X_train.shape[1] == x_predict.shape[1],\
+                "The size must be valid"
+        assert self._X_train is not None and self._y_train is not None,\
+                "The process fit should do before predict"
+
+        y_predict = [self._doPredict(x) for x in x_predict]
+        return y_predict
+
+    def _doPredict(self,x):
+        '''check'''
+        assert x.shape[1] == self._X_train.shape[1],\
+                "The size must be valid"
+
+        distances = [sqrt(np.sum((x - self._X_train)**2)) for x_train in self._X_train]
+        sortedIndex = np.argsort(distances)
+        topK = [self._y_train[i] for i in sortedIndex[:self.k]]
+        return Counter(topK).most_common(1)[0][0]
+
+    def __repr__(self):
+        return "KNeighborsClassifier()"
+
+
+
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Machine-Learning
 **In order to deepen the understanding of Machine_learning algorithm,package a
 tool by myself.**
-Includes:
+
+
+### _Includes:(Base on Python)_
 
 
 1.KNN(K-Neighbors-Classifier)
@@ -36,6 +38,7 @@ Includes:
 
 11.SVM
 
+`to read the personal understanding of most of algorithm,to visit https://blog.csdn.net/qq_41979922`
 
 
 Thanks for your reading and please give your advice if you think exist something wrong.
